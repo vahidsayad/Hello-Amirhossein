1f8fedc0af004716ccb8e88f01d2dc80db323afc
Vahid Sayad <vahidsayad@gmail.com>
2022-06-08 11:53:23 +0430

    Initial Commit

---
+++ Hello Amirhossein/Hello_AmirhosseinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_AmirhosseinApp.swift
+//  Hello Amirhossein
+//
+//  Created by Vahid Sayad on 8/6/2022 .
+//
+
+import SwiftUI
+
+@main
+struct Hello_AmirhosseinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
